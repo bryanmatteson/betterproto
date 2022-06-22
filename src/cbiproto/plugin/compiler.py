96fa2f2022979ed615e2156c21fbdaab05db26c7
bryan.matteson <bryan.matteson@cbinsights.com>
2022-06-22 18:21:26 -0500

    fixed import in template

--- src/cbiproto/plugin/compiler.py
+++ src/cbiproto/plugin/compiler.py
@@ -37,7 +37,11 @@ def with_header(code: str) -> str:
 
 def outputfile_compiler(output_file: OutputTemplate) -> str:
     templates_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "templates"))
-    env = jinja2.Environment(trim_blocks=True, lstrip_blocks=True, loader=jinja2.FileSystemLoader(templates_folder),)
+    env = jinja2.Environment(
+        trim_blocks=True,
+        lstrip_blocks=True,
+        loader=jinja2.FileSystemLoader(templates_folder),
+    )
     template = env.get_template("template.py.j2")
 
     code = template.render(output_file=output_file)
